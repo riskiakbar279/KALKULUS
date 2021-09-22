2bcce7993ca162bfad8c1baf9e1000d927bc3ec5
riskiakbar279 <70522253+riskiakbar279@users.noreply.github.com>
2021-09-22 17:43:31 +0700

    Add files via upload

---
+++ module/loading.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#warna
+g='\033[1;92m'
+c='\033[1;36m'
+r='\033[1;91m'
+re='\033[0m'
+
+clear
+echo $c' Tunggu Sebentar'$re'......\'
+echo $g'•---------------------• '
+echo $g'¦      '$c' loading      '$g' ¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'......|'
+echo $g'•---------------------• '
+echo $g'¦'$re'123'$c'      loading      '$g' ¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'....../'
+echo $g'•---------------------• '
+echo $g'¦'$re'12345'$c'     loading      '$g' ¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'......-'
+echo $g'•---------------------• '
+echo $g'¦'$re'12345678910'$c'    loading    '$g'   ¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'......\'
+echo $g'•---------------------• '
+echo $g'¦'$re'123456789101112131415'$c'   loading       '$g'¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'......|'
+echo $g'•---------------------• '
+echo $g'¦'$re'1234567891011121314151617181920'$c' loading      '$g' ¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'....../'
+echo $g'•---------------------• '
+echo $g'¦'$re'12345678910111213141516171819202122232425'$c'loading'$g'       ¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'......-'
+echo $g'•---------------------• '
+echo $g'¦'$re'123456789101112131415161718192021222324252627282930'$c'loading      '$g' ¦'
+echo $g'•---------------------• '
+sleep 0.1
+clear
+echo $c' Tunggu Sebentar'$re'......!'
+echo $g'•---------------------•'
+echo $g'¦'$re'TERVERIFIKASI'$g'¦'
+echo $g'•---------------------•'
